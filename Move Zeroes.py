ce31f9f6002180e7744db0586136bbe7e64da74a
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-20 02:14:37 +0500

    React Practice

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -7,11 +7,14 @@ class Solution(object):
         """
         if len(nums)==1:
             return nums
+        for i in range(len(nums)):
+            if(nums[i]==0):
+                nums[-1:],nums[i]==nums[i]
         count=len(nums)
-        print(nums[-1:])
+        print(nums)
 if __name__ == "__main__":
     sol = Solution()
-    nums = [0]
+    nums = [0,1,0,3,12]
     result = sol.moveZeroes(nums)
     print(result)
     
