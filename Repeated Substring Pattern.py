f64e827d9e92ba07121f74ee7f2a68cef9f842a1
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-19 23:01:02 +0500

    React Practice

--- Repeated Substring Pattern.py
+++ Repeated Substring Pattern.py
@@ -5,18 +5,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        count_s = Counter(s)
-        
         substring = ""
         if(len(s)<=1):
             return False
-        for char in count_s:
-            substring+=char
-            
+        substring= s[:len(s)//2]
+        print(substring)    
         substring2 = ""
-        while len(substring2)<len(s):
+        for i in range(len(substring)):
+            if substring2 == s:
+                return True
+            while len(substring2) < len(s):
+               substring2+=substring
             
-            substring2+=substring
         print(substring2)
         if substring2 == s:
             return True
