3a482c0ed8fec61d1fe72cb4609d5adf8f19f8af
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-20 13:54:43 +0500

    React Practice

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -1,32 +1,22 @@
-#not Complete Yet
-from collections import Counter
 class Solution(object):
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        :rtype: None (in-place modification)
         """
-        if len(nums)<=1:
-            return nums
-        counter_0 = Counter(nums)
-        
-        movecount = 0
+        non_zero_index = 0
         for i in range(len(nums)):
-            if nums[i]==0  and movecount!=counter_0[0]:
-                b=0
-                for j in range(i+1,len(nums)):
-                        print(j)
-                        print(i+b)
-                        nums[i+b],nums[j]=nums[j],nums[i+b]
-                        print(f"nums in iteration {nums}")
-                        b+=1
-                movecount+=1
-            if(movecount==counter_0[0]):
-                break
-        return nums
+            if nums[i] != 0:
+                nums[non_zero_index] = nums[i]
+                non_zero_index += 1
+        print(nums)
+        for i in range(non_zero_index, len(nums)):
+            nums[i] = 0
+        print(nums)
+        return nums 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [0,0,1]
+    nums = [0,1,0,3,12]
     result = sol.moveZeroes(nums)
     print(result)
     
