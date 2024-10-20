60a04fe4ce4fd42b08bbf6a030ec70a6ba4b1ece
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-20 12:00:15 +0500

    React Practice

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -9,16 +9,19 @@ class Solution(object):
         if len(nums)==1:
             return nums
         counter_0 = Counter(nums)
-        print(counter_0)
+        
         movecount = 0
         for i in range(len(nums)):
-            if nums[i]==0 :
+            if nums[i]==0  and movecount!=counter_0[0]:
                 for j in range(len(nums)-1,-1,-1):
                     if nums[j]!=0:
-                        nums[i],nums[j]=nums[j],nums[i]
+                        nums[i]=nums[j]
                         print(f"nums in iteration {nums}")
                         movecount+=1
                         break
+            if(movecount==counter_0[0]):
+                break
+            print(nums)
         count=len(nums)
         print(nums)
 if __name__ == "__main__":
