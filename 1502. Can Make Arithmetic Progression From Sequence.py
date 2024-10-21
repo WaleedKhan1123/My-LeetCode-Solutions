ed0377ced51c249bf78ff3e853dd0c499c969b81
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-22 01:26:52 +0500

    leetcode solution

--- 1502. Can Make Arithmetic Progression From Sequence.py
+++ 1502. Can Make Arithmetic Progression From Sequence.py
@@ -4,9 +4,19 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        return "fuck"
+        if len(arr)<=1:
+            return False
+        arr = sorted(arr)
+        difference = arr[1]-arr[0]
+        print(difference)
+        for i in arr:
+            difference+=i
+            print(i)
+            if(difference not in arr):
+                return False
+        return True
 if __name__ == "__main__":
     sol = Solution()
-    arr = [5,3,1]
+    arr = [0,1,2]
     result = sol.canMakeArithmeticProgression(arr)
     print(result)        
