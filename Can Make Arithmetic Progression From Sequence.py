675ece3a3f60afc349af29d5fc2d8fe890a9c012
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-22 01:12:32 +0500

    leetcode solution

---
+++ Can Make Arithmetic Progression From Sequence.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canMakeArithmeticProgression(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        return "fuck"
+if __name__ == "__main__":
+    sol = Solution()
+    arr = [5,3,1]
+    result = sol.canMakeArithmeticProgression(arr)
+    print(result)        
