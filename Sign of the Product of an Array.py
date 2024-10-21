3961cbd04e0b082c6e2e08b849a95fc210189a43
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-21 23:46:42 +0500

    leetcode solution

--- Sign of the Product of an Array.py
+++ Sign of the Product of an Array.py
@@ -4,8 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        product = 1
         for i in nums:
-            print(nums[i]*nums[(i+1)])
+            product*= i
+        def signFunc(product):
+            if product>0:
+                return 1
+            elif product ==0:
+                return 0
+            else:
+                return -1
+        result = signFunc(product)
+        return result
 if __name__ == "__main__":
     sol = Solution()
     nums = [-1,-2,-3,-4,3,2,1]
