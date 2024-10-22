acd5d775e4832307359380b83c99c8998b61f89d
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-23 01:35:29 +0500

    LeetCode Solutions

--- 896. Monotonic Array.py
+++ 896. Monotonic Array.py
@@ -4,12 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        same = all(x==nums[0]for x in nums)
+        if same:
+            return False
         for i in range(len(nums)):
-            print(nums[i])
+            if i != (len(nums)-1):
+                j=i+1
+                if nums[i]<=nums[j]:
+                     continue
+                else:
+                    return False
+        return True
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [1,2,2,3]
+    nums = [0,0,0,0]
     result = sol.isMonotonic(nums)
     print(result)
     
