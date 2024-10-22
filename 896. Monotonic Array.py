c5faeea72f71197814b1ab67b232683481e6ccbe
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-23 01:07:08 +0500

    LeetCode Solutions

---
+++ 896. Monotonic Array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1,2,2,3]
+    result = sol.isMonotonic(nums)
+    print(result)
+    
+    
+    
