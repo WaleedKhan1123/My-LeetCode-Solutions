a43ed5e577a79b50d669bae752304a35fe369a4c
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-24 01:56:57 +0500

    LeetCode Solutions

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return 3
+if __name__ == "__main__":
+    sol = Solution()
+    s = "III"
+    result = sol.romanToInt(s)
+    print(result)
