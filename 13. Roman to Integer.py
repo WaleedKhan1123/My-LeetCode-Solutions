2a9e3b916e537cbcb1940606675c3c705846557f
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-25 00:38:49 +0500

    LeetCode Solutions

--- 13. Roman to Integer.py
+++ 13. Roman to Integer.py
@@ -1,30 +1,30 @@
-# not completed yet
 class Solution(object):
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        romans = ['I','V','X','L','C','D','M']
-        numbers= ['1','5','10','50','100','500','1000']
-        numlist = []
-        for i in range(len(s)):
-            for j in range(len(romans)):
-               if s[i] == romans[j]:
-                   if (s[i] =='V' and s[i-1]=='I') or (s[i] =='X' and s[i-1]=='I') :
-                       numlist.append(int(numbers[j])-(int(numbers[0])*2))
-                   elif (s[i] =='L' and s[i-1]=='X') or (s[i] =='C' and s[i-1]=='X') :
-                       numlist.append(int(numbers[j])-(int(numbers[2])*2))
-                   elif (s[i] =='D' and s[i-1]=='C') or (s[i] =='M' and s[i-1]=='C')  :
-                       numlist.append(int(numbers[j])-(int(numbers[4])*2))
-                   else:
-                       numlist.append(int(numbers[j])) 
-        num=0
-        for i in range(len(numlist)):
-                num+=numlist[i]
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        num = 0
+        n = len(s)
+        for i in range(n):
+            if i < n - 1 and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                num -= roman_to_int[s[i]]
+            else:
+                num += roman_to_int[s[i]]
+
         return num
 if __name__ == "__main__":
     sol = Solution()
-    s = "MCMXCIV"
+    s = "MMMCDXC"
     result = sol.romanToInt(s)
     print(result)
