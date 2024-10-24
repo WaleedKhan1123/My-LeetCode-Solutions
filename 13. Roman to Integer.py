532e91fbc6161ce878a3e2ff985259b470763e1c
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-25 00:30:24 +0500

    LeetCode Solutions

--- 13. Roman to Integer.py
+++ 13. Roman to Integer.py
@@ -11,18 +11,12 @@ class Solution(object):
         for i in range(len(s)):
             for j in range(len(romans)):
                if s[i] == romans[j]:
-                   if s[i] =='V' and s[i-1]=='I'  :
-                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
-                   elif s[i] =='X' and s[i-1]=='I'  :
-                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
-                   elif s[i] =='L' and s[i-1]=='X'  :
-                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
-                   elif s[i] =='V' and s[i-1]=='L'  :
-                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
-                   elif s[i] =='C' and s[i-1]=='D'  :
-                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
-                   elif s[i] =='D' and s[i-1]=='M'  :
-                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
+                   if (s[i] =='V' and s[i-1]=='I') or (s[i] =='X' and s[i-1]=='I') :
+                       numlist.append(int(numbers[j])-(int(numbers[0])*2))
+                   elif (s[i] =='L' and s[i-1]=='X') or (s[i] =='C' and s[i-1]=='X') :
+                       numlist.append(int(numbers[j])-(int(numbers[2])*2))
+                   elif (s[i] =='D' and s[i-1]=='C') or (s[i] =='M' and s[i-1]=='C')  :
+                       numlist.append(int(numbers[j])-(int(numbers[4])*2))
                    else:
                        numlist.append(int(numbers[j])) 
         num=0
@@ -31,6 +25,6 @@ class Solution(object):
         return num
 if __name__ == "__main__":
     sol = Solution()
-    s = "IX"
+    s = "MCMXCIV"
     result = sol.romanToInt(s)
     print(result)
