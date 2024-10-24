a87ec5f0df5c7528da67da3f28240cfb1625c9d5
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-24 23:46:55 +0500

    LeetCode Solutions

--- 13. Roman to Integer.py
+++ 13. Roman to Integer.py
@@ -5,9 +5,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        return 3
+        romans = ['I','V','X','L','C','D','M']
+        numbers= ['1','5','10','50','100','500','1000']
+        numlist = []
+        for i in range(len(s)):
+            for j in range(len(romans)):
+               if s[i] == romans[j]:
+                   numlist.append(numbers[j]) 
+        num=0
+        for i in numlist:
+            num+=int(i)
+        return num
 if __name__ == "__main__":
     sol = Solution()
-    s = "III"
+    s = "XXVII"
     result = sol.romanToInt(s)
     print(result)
