e289ad38eaa63b09019d7aaceacbd4b4983ad144
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-25 00:21:36 +0500

    LeetCode Solutions

--- 13. Roman to Integer.py
+++ 13. Roman to Integer.py
@@ -11,13 +11,26 @@ class Solution(object):
         for i in range(len(s)):
             for j in range(len(romans)):
                if s[i] == romans[j]:
-                   numlist.append(numbers[j]) 
+                   if s[i] =='V' and s[i-1]=='I'  :
+                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
+                   elif s[i] =='X' and s[i-1]=='I'  :
+                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
+                   elif s[i] =='L' and s[i-1]=='X'  :
+                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
+                   elif s[i] =='V' and s[i-1]=='L'  :
+                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
+                   elif s[i] =='C' and s[i-1]=='D'  :
+                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
+                   elif s[i] =='D' and s[i-1]=='M'  :
+                       numlist.append(int(numbers[j])-(int(numbers[j-1])*2))
+                   else:
+                       numlist.append(int(numbers[j])) 
         num=0
         for i in range(len(numlist)):
-            num+=int(numlist[i])
+                num+=numlist[i]
         return num
 if __name__ == "__main__":
     sol = Solution()
-    s = "XXVII"
+    s = "IX"
     result = sol.romanToInt(s)
     print(result)
