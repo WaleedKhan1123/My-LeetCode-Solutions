fc523defa7183ed288638c3e822d6801f0b0a803
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-01 04:27:07 +0500

    LeetCode Solutions

--- 709. To Lower Case.py
+++ 709. To Lower Case.py
@@ -4,8 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        String = {'A':'a','B':'b','C':'c','D':'d','E':'e','F':'f','G':'g','H':'h','I':'i','J':'j','K':'k','L':'l','M':'m','N':'n','O':'o','P':'p','Q':'q','R':'r','S':'s','T':'t','U':'u','V':'v','X':'x','Y':'y','Z':'z'}
-        print(len(String))
+        return s.lower()
 if __name__ == "__main__":
     sol = Solution()
     s = "Hello"
