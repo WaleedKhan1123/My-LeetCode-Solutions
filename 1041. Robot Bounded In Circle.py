fbb81b5510257cb8da707077c2eab6380bf8be6b
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-02 23:02:25 +0500

    LeetCode Solutions

--- 1041. Robot Bounded In Circle.py
+++ 1041. Robot Bounded In Circle.py
@@ -4,6 +4,16 @@ class Solution(object):
         :type instructions: str
         :rtype: bool
         """
+        
+        Directions = {
+            
+            "North":0,
+            "South":0,
+            "West":0,
+            "East":0
+        }
+        
+        
 if __name__ == "__main__":
     sol = Solution()
     instructions = "GGLLGG"
