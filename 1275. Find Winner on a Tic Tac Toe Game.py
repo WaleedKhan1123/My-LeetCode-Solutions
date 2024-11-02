805d9e7cbd46b192c88ecb9028712c217f26d381
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-02 18:53:47 +0500

    LeetCode Solutions

--- 1275. Find Winner on a Tic Tac Toe Game.py
+++ 1275. Find Winner on a Tic Tac Toe Game.py
@@ -4,6 +4,12 @@ class Solution(object):
         :type moves: List[List[int]]
         :rtype: str
         """
+        
+        a_moves = [moves[move] for move in range(0,len(moves),2)]
+        b_moves = [moves[move] for move in range(1,len(moves),2)]
+        
+        print(a_moves)
+        print(b_moves)
 if __name__ == "__main__":
     sol = Solution()
     moves = [[0,0],[2,0],[1,1],[2,1],[2,2]]
