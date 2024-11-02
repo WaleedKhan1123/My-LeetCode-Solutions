b8fc3adcf41f64a7fde1b5fefaba3c5e63d4c003
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-02 22:57:40 +0500

    LeetCode Solutions

---
+++ 1041. Robot Bounded In Circle.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isRobotBounded(self, instructions):
+        """
+        :type instructions: str
+        :rtype: bool
+        """
+if __name__ == "__main__":
+    sol = Solution()
+    instructions = "GGLLGG"
+    result = sol.isRobotBounded(instructions)
+    
+    print(result)
+    
