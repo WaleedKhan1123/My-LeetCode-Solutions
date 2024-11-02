f5339099eff69c32377617a135584fee75ad7bf5
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-02 19:24:17 +0500

    LeetCode Solutions

--- 1275. Find Winner on a Tic Tac Toe Game.py
+++ 1275. Find Winner on a Tic Tac Toe Game.py
@@ -13,8 +13,8 @@ class Solution(object):
         win4= [[1,0],[1,1],[1,2]]
         win5= [[2,0],[2,1],[2,2]]
         win6= [[0,2],[1,1],[2,0]]
-        win7= [[2,0],[2,1],[2,2]]
-        win8= [[0,1],[1,1],[2,2]]
+        win7= [[0,2],[1,2],[2,2]]
+        win8= [[0,1],[1,1],[2,1]]
         if all(move in a_moves for move in win1 ) or all(move in a_moves for move in win2 ) or all(move in a_moves for move in win3 ) or all(move in a_moves for move in win4 ) or all(move in a_moves for move in win5 ) or all(move in a_moves for move in win6 ) or all(move in a_moves for move in win7 ) or all(move in a_moves for move in win8 ):
          return "A"
         elif all(move in b_moves for move in win1 ) or all(move in b_moves for move in win2 ) or all(move in b_moves for move in win3 ) or all(move in b_moves for move in win4 ) or all(move in b_moves for move in win5 ) or all(move in b_moves for move in win6 ) or all(move in b_moves for move in win7 ) or all(move in b_moves for move in win8 ):
@@ -24,10 +24,10 @@ class Solution(object):
                 return "Draw"
             return "Pending"
         
-        [[1,2],[1,1],[0,2],[2,2],[2,1],[0,1],[0,0]]
+        
 if __name__ == "__main__":
     sol = Solution()
-    moves = [[0,0],[2,0],[1,1],[2,1],[2,2]]
+    moves = [[0,0],[2,2],[1,0],[2,0],[0,1],[1,2],[1,1],[0,2]]
     result = sol.tictactoe(moves) 
     
     print(result)
