1e9e3d8c1999782cbcf9ef5a680dcecdea950de6
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-02 18:59:41 +0500

    LeetCode Solutions

--- 1275. Find Winner on a Tic Tac Toe Game.py
+++ 1275. Find Winner on a Tic Tac Toe Game.py
@@ -7,7 +7,14 @@ class Solution(object):
         
         a_moves = [moves[move] for move in range(0,len(moves),2)]
         b_moves = [moves[move] for move in range(1,len(moves),2)]
-        
+        win1= [[0,0],[1,1],[2,2]]
+        win2= [[0,0],[1,0],[2,0]]
+        win3= [[0,0],[0,1],[0,2]]
+        win4= [[1,0],[1,1],[1,2]]
+        win5= [[2,0],[2,1],[2,2]]
+        win6= [[0,2],[1,1],[2,0]]
+        win7= [[2,0],[2,1],[2,2]]
+        win8= [[0,1],[2,1],[2,2]]
         print(a_moves)
         print(b_moves)
 if __name__ == "__main__":
