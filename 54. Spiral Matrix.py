9e54433f95cf80db7ab946d8f899b8e21c084907
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-04 00:19:38 +0500

    Leetcode Solutions

---
+++ 54. Spiral Matrix.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+if __name__ == "__main__":
+    sol = Solution()
+    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    result = sol.spiralOrder(matrix)
+    print(result)
