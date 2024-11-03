6d9a39cd3e683286be5425bc71630ca30e564bb2
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-04 00:25:08 +0500

    Leetcode Solutions

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -4,6 +4,13 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
+        spiral = []
+        
+        for m in matrix:
+            
+            for i in m:
+                spiral.append(i)
+        print(spiral)
 if __name__ == "__main__":
     sol = Solution()
     matrix = [[1,2,3],[4,5,6],[7,8,9]]
