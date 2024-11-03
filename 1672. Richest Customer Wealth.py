da0a876001ae0c4d41685ddf187e6e7038263d8d
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-03 17:26:26 +0500

    LeetCode Solutions

--- 1672. Richest Customer Wealth.py
+++ 1672. Richest Customer Wealth.py
@@ -4,12 +4,26 @@ class Solution(object):
         :type accounts: List[List[int]]
         :rtype: int
         """
+        richList = []
+        wealth = 0
+        for a in accounts:
+             
+            
+            for i in range(len(a)):
+               wealth+=a[i]      
+            richList.append(wealth)
+            wealth=0
+        richest = max(richList)
+        try:
+         if len(richest)>1:
+            return richest[0]
+        except:
+            return richest    
         
-        
-        
+    
 if __name__ == "__main__":
     sol = Solution()
-    accounts = [[1,2,3],[3,2,1]]
+    accounts = [[1,5],[7,3],[3,5]]
     result = sol.maximumWealth(accounts)
     
     print(result)
