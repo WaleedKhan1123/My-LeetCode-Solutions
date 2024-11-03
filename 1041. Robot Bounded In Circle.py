214bbcbc57ad882814e7651dfc25c4680a270780
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-03 17:07:38 +0500

    LeetCode Solutions

--- 1041. Robot Bounded In Circle.py
+++ 1041. Robot Bounded In Circle.py
@@ -4,22 +4,64 @@ class Solution(object):
         :type instructions: str
         :rtype: bool
         """
-        n = len(instructions)
-        pos = (0, 0)
-        directions = [(0, 1), (-1, 0), (0, -1), (1, 0)]
-        curr = 0
-        for i in range(n):
-            if instructions[i]=='G':
-                pos = (pos[0]+directions[curr][0], pos[1]+directions[curr][1])
-            elif instructions[i]=='L':
-                curr = (curr + 1) % 4
-            elif instructions[i]=='R':
-                curr = (curr - 1) % 4
-        return pos==(0, 0) or curr >0
+        
+        Directions = {
+            
+            "North":1,
+            "West":0,
+            "East":0,
+            "South":0
+        }
+        coordinates = [0,0]
+        Circle = 0
+        c = 0
+        while c != 10:
+            for i in instructions:
+                if i == "G":
+                    if Directions["North"]==1:
+                        coordinates[1]=coordinates[1]+1 
+                    elif Directions["West"]==1:
+                        coordinates[0]=coordinates[0]-1
+                    elif Directions["South"] ==1:
+                        coordinates[1]=coordinates[1]-1
+                    elif Directions["East"] ==1:
+                        coordinates[0]=coordinates[0]+1
+                                        
+                elif i =="L":
+                    if Directions["North"]==1:
+                        Directions["North"] = 0
+                        Directions["West"] = 1
+                    elif Directions["West"]==1:
+                        Directions["West"] = 0
+                        Directions["South"] = 1
+                    elif Directions["South"] ==1:
+                        Directions["South"] = 0
+                        Directions["East"] =1
+                    elif Directions["East"] ==1:
+                        Directions["East"] = 0
+                        Directions["North"] =1
+                elif i == "R":
+                    if Directions["North"]==1:
+                        Directions["North"] = 0
+                        Directions["East"] = 1
+                    elif Directions["East"]==1:
+                        Directions["East"] = 0
+                        Directions["South"] = 1
+                    elif Directions["South"] ==1:
+                        Directions["South"] = 0
+                        Directions["West"] =1
+                    elif Directions["West"] ==1:
+                        Directions["West"] = 0
+                        Directions["North"] =1
+                           
+            if coordinates[0] == 0 and coordinates [1] ==0:
+                return True
+            c +=1
+            
+        return False
 if __name__ == "__main__":
     sol = Solution()
     instructions = "GG"
-    result = sol.isRobotBounded(instructions)
+    result = sol.isRobotBounded(instructions) 
     
     print(result)
-    
