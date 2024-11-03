89cff3616b34b775f5228f8617b9d2a9a454b606
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-04 00:51:00 +0500

    Leetcode Solutions

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -5,14 +5,38 @@ class Solution(object):
         :rtype: List[int]
         """
         spiral = []
-        
-        for m in matrix:
+        length = len(matrix)
+        reverse = len(matrix[0])-1 
+        if length==1:
+            return matrix[0]
+        if length ==2:
+           for i in range(length):
+               for b in range(len(matrix[0])):
+                 
+                if i==0:
+                    spiral.append(matrix[i][b])
+                else:
+                    spiral.append(matrix[i][reverse])
+                    reverse-=1
+           return spiral             
+        reverse = len(matrix[0])-1
+        for i in range(length):
+            for b in range(len(matrix[0])):
+                if i==1:
+                    spiral.append(matrix[i][-1])
+                    break
+                elif i==0:
+                    spiral.append(matrix[i][b])
+                else:
+                    spiral.append(matrix[i][reverse])
+                    reverse-=1
+                       
+        for i in range(0,len(matrix[1])-1):
             
-            for i in m:
-                spiral.append(i)
-        print(spiral)
+            spiral.append(matrix[1][i])
+        return spiral
 if __name__ == "__main__":
     sol = Solution()
-    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    matrix = [[2,5,8],[4,0,-1]]
     result = sol.spiralOrder(matrix)
     print(result)
