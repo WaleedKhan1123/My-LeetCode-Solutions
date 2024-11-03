de314bb8bce5e36312329f317113d240508db01c
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-03 17:14:46 +0500

    LeetCode Solutions

---
+++ 1672. Richest Customer Wealth.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        
+        
+        
+if __name__ == "__main__":
+    sol = Solution()
+    accounts = [[1,2,3],[3,2,1]]
+    result = sol.maximumWealth(accounts)
+    
+    print(result)
