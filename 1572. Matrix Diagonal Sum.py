fbaecc341cc367eb125c8d4503526c0110028245
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-03 21:29:27 +0500

    LeetCode Solutions

--- 1572. Matrix Diagonal Sum.py
+++ 1572. Matrix Diagonal Sum.py
@@ -4,6 +4,19 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: int
         """
+        sum=0
+        second_i=0
+        for i in range(len(mat[0])):
+            sum+=mat[i][i]
+        print(sum)
+        for i in range(len(mat[0]) - 1, -1, -1):
+            print(i)
+            sum+=mat[second_i][i]
+            second_i+=1
+            print(second_i)
+        
+        print(sum)
+        
 if __name__ == "__main__":
     sol = Solution()
     
