64a2f166bb2091e7fde20860cb556e2f6ab90e8d
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-03 21:07:41 +0500

    LeetCode Solutions

---
+++ 1572. Matrix Diagonal Sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def diagonalSum(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: int
+        """
+if __name__ == "__main__":
+    sol = Solution()
+    mat = [[1,2,3],
+           [4,5,6],
+           [7,8,9]]
+    result = sol.diagonalSum(mat)
+    
+    print(result)
