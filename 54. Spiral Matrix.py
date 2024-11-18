113c5f897a88fdd27d992bb2d7912d9204d0a1ac
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-18 16:02:10 +0500

    Leet code Solutions

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -12,34 +12,32 @@ class Solution(object):
         print(f"matrix is {m} x {n}")
         newMatrix = []
         traverse = 0
+        reverse = 0
         if m == 1:
             return newMatrix+matrix[0]
         x=0
         for i in matrix:
           x+=1
-          if traverse==0:
+          if traverse==0 and reverse==0:
                for j in i:
                    newMatrix.append(j)
                
                traverse=1
-          elif traverse==1:
-              for k in range(m):
+          elif traverse==1 and reverse ==0:
+              for k in range(1,m):
                   print(f"k is {k}")
                   newMatrix.append(matrix[k][n-1])
-              
+                  
               m-= 1
+              
           if x == 2:
            break
        
                
           
            
-            
-        
-        
         print(f"Spiral Matix is {newMatrix}")
        
-        
 if __name__ == "__main__":
     sol = Solution()
     matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
