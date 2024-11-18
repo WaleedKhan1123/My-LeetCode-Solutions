a032dfc6f059ec85d3e859d4a2b01e909937a110
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-18 16:30:10 +0500

    Leet code Solutions

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -4,52 +4,41 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
+        if not matrix or not matrix[0]:
+            return []
         
-         
+        m, n = len(matrix), len(matrix[0])  
+        top, bottom, left, right = 0, m - 1, 0, n - 1
+        result = []
         
-        m  = len(matrix)
-        n  = len(matrix[0])
-        print(f"matrix is {m} x {n}")
-        newMatrix = []
-        traverse = 0
-        reverse = 0
-        countRows = 0
-        if m == 1:
-            return newMatrix+matrix[0]
-        x=0
-        for i in matrix:
-          x+=1
-          
-          if traverse==0 and reverse==0:
-               for j in i:
-                   newMatrix.append(j)
-               
-               traverse=1
-               countRows+=1
-          elif traverse==1 and reverse ==0:
-              for k in range(countRows,m):
-                  print(f"k is {k}")
-                  newMatrix.append(matrix[k][n-1])
-                  
-              m-= 1
-              n-=1
-              reverse=1
-          elif traverse==0 and reverse ==1:
-              
-              
-          elif traverse==1 and reverse==1:
-              
-              
-          if x == 2:
-           break
-       
-               
-          
-           
-        print(f"Spiral Matix is {newMatrix}")
-       
+        while top <= bottom and left <= right:
+
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1  
+            
+            
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1  
+            
+            
+            if top <= bottom:  
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1  
+            
+            
+            if left <= right:  
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1  
+        
+        print(f"Spiral Matrix is {result}")
+        return result
+
 if __name__ == "__main__":
     sol = Solution()
-    matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
     result = sol.spiralOrder(matrix)
     print(result)
