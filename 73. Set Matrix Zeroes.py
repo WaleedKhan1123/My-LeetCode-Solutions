aca5f7167b1d98848287be54dfc78fbb57816ed6
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-25 10:44:06 +0500

    LeetCode Solution

---
+++ 73. Set Matrix Zeroes.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+
+if __name__ == "__main__":
+    sol = Solution()
+    matrix = [1,2,3]
+    result = sol.setZeroes(matrix)
+    print(result)       
