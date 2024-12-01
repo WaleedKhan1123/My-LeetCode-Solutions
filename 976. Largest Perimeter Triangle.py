db235e3275e558e4c01ea3f04bba8b6c492a831b
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-02 00:13:03 +0500

    leetcode solution

---
+++ 976. Largest Perimeter Triangle.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def largestPerimeter(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+if __name__ == "__main__":
+
+    sol = Solution()
+    
+    nums = [2,1,2]
+    
+    result = sol.largestPerimeter(nums)
+    
+    print(result)
