f95457c011684845c1e5d93439b4b41a1b8e1ce3
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-01 18:23:05 +0500

    leetcode solution

--- 860. Lemonade Change.py
+++ 860. Lemonade Change.py
@@ -4,33 +4,32 @@ class Solution(object):
         :type bills: List[int]
         :rtype: bool
         """
-        change = []
-        for b in bills:
-            if b ==5:
-                change.append(5)
-            elif b==10:
-                try:
+        change = {'5':0,'10':0,'20':0}
 
-                    change.remove(5)
-                    change.append(10)
-                except:
+        for b in bills:
+            if b == 5:
+                change['5']+=1
+            elif b==10:
+                if change['5']>0:
+                    change['10']+=1
+                    change['5']-=1
+                else:
                     return False
-            else:
-                try:
-                    change.remove(10)
-                    change.remove(5)
-                    change.append(20)
-                except:
-                    try:
-                        change.remove(5)
-                        change.remove(5)
-                        change.remove(5)
-                    except:
-                        return False
+                
+            else :
+                if change['5']>0 and change['10']>0:
+                    change['10']-=1
+                    change['5']-=1
+                    change['20'] +=1
+                elif change['5']>=3:
+                    change['5']-=3
+                else:
+                    return False
         return True
+        
     
 if __name__ == "__main__":
     sol = Solution()
-    bills = [5,5,10,20,5,5,5,5,5,5,5,5,5,10,5,5,20,5,20,5]
+    bills = [5,5,5,20]
     result = sol.lemonadeChange(bills)
     print(result)     
