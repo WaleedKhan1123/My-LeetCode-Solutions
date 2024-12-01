88b1dd1ff4a1d83eea35085660e9131ad411358e
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-01 15:43:03 +0500

    leetcode solution

--- 1491. Average Salary Excluding the Minimum and Maximum Salary.py
+++ 1491. Average Salary Excluding the Minimum and Maximum Salary.py
@@ -4,20 +4,15 @@ class Solution(object):
         :type salary: List[int]
         :rtype: float
         """
-        min ,max= -1,0
+        low,high= min(salary),max(salary)
         avg=[]
         for i in salary:
-            if i <min:
-                min = i
-            elif i>max:
-                max=i
-            else:
+            if i !=high or i!=low:
                 avg.append(i)
-
-        print(avg)
         return sum(avg)/len(avg)
     
 if __name__ == "__main__":
+
     sol = Solution()
     
     salary = [4000,3000,1000,2000]
