097bdd88f166ac41fe38699a4d55142c2b751f01
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-01 15:24:54 +0500

    leetcode solution

---
+++ 1491. Average Salary Excluding the Minimum and Maximum Salary.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def average(self, salary):
+        """
+        :type salary: List[int]
+        :rtype: float
+        """
+if __name__ == "__main__":
+    sol = Solution()
+    
+    salary = [4000,3000,1000,2000]
+    
+    result = sol.average(salary)
+    
+    print(result)
