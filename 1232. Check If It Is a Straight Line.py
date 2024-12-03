3f3867e72f5ecb49bae21d945dd4afd9832a0810
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-03 12:01:28 +0500

    leetcode solution

--- 1232. Check If It Is a Straight Line.py
+++ 1232. Check If It Is a Straight Line.py
@@ -4,21 +4,29 @@ class Solution(object):
         :type coordinates: List[List[int]]
         :rtype: bool
         """
-        distance_x = coordinates[1][0] - coordinates[0][0]
-        distance_y = coordinates[1][1] - coordinates[0][1]
+        x = []
+        y = []
+        for c in coordinates:
+            x.append(c[0])
+            y.append(c[1])
+        x.sort()       
+        y.sort()
+        print(y)
+        print(x)
+        distance_x = x[1] - x[0]
+        distance_y = y[1] - y[0]
         for c in range(len(coordinates)-1):
             
-            if coordinates[c+1][0]-coordinates[c][0]==distance_x and coordinates[c+1][1]-coordinates[c][1]==distance_y:
+            if x[c+1]-x[c]==distance_x and y[c+1]-y[c]==distance_y:
                 continue
             else:
                 return False
 
         return True
-
-        
+    
 if __name__ == "__main__":
     sol = Solution()
-    coordinates = [[1,2],[2,3],[3,5]]
+    coordinates = [[0,0],[0,1],[0,-1]]
     result = sol.checkStraightLine(coordinates) 
     
     print(result)
