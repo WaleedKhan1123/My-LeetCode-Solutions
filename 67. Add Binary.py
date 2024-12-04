e994cba070c7e655471a1ae63c9265329a6c0764
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-05 01:56:17 +0500

    leetcode solution

--- 67. Add Binary.py
+++ 67. Add Binary.py
@@ -5,6 +5,15 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
+        dec = {
+            '1': 1, 
+            '11': 2,
+            '100': 4,
+            '1000': 8,
+            '10000': 16,
+            '100000': 32
+        }
+
 if __name__ == "__main__":
     sol = Solution()
     a = "11"
