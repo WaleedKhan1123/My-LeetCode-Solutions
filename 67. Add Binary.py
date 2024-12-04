85368041acd442fce35adf616f67e881906ef5bd
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-05 01:51:47 +0500

    leetcode solution

---
+++ 67. Add Binary.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+if __name__ == "__main__":
+    sol = Solution()
+    a = "11"
+    b = "1"
+    result = sol.addBinary(a,b)
+    print(result)
