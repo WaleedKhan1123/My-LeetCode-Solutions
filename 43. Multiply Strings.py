a8c3644385fbc4ecb7d48eb5e04b9d0d09d4d63d
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-09 16:48:51 +0500

    leetcode solution

---
+++ 43. Multiply Strings.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    num1 = "2", num2 = "3"
+    result = sol.multiply(num1,num2)
+    print(result)
