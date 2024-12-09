02f7a33de02c88d407fed35098ebd658bf06a328
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-09 16:52:51 +0500

    leetcode solution

--- 43. Multiply Strings.py
+++ 43. Multiply Strings.py
@@ -5,8 +5,21 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
+        num = {
+            '0':0,
+            '1':1,
+            '2':2,
+            '3':3,
+            '4':4,
+            '5':5,
+            '6':6,
+            '7':7,
+            '8':8,
+            '9':9
+        }
         
-        print(num1,num2)
+        numa = 0
+        numb = 0
 
 if __name__ == "__main__":
     sol = Solution()
