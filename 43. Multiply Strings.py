6c26bede50c12da54a7abe72867c4a2fac208e0e
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-16 12:08:43 +0500

    leetcode Solutions

--- 43. Multiply Strings.py
+++ 43. Multiply Strings.py
@@ -18,6 +18,18 @@ class Solution(object):
             '8':8,
             '9':9
         }
+        nums = {
+            '0':'0',
+            '1':'1',
+            '2':'2',
+            '3':'3',
+            '4':'4',
+            '5':'5',
+            '6':'6',
+            '7':'7',
+            '8':'8',
+            '9':'9'
+        }
         numx=[]
         numa=0
         numb=0
@@ -29,22 +41,28 @@ class Solution(object):
         c=0
         num_digits = 1
         num3=0
-        
+        num4=0
             
         for n in range(len(num1)-1):
              if n ==0:
               num3 = num[num1[n]] * (10 ** num_digits) + num[num1[n+1]] 
              else:
                 num3 = num3 * (10 ** num_digits) + num[num1[n+1]]
-                  
-        print(num3)        
+        
+        for n in range(len(num2)-1):
+             if n ==0:
+              num4 = num[num2[n]] * (10 ** num_digits) + num[num2[n+1]] 
+             else:
+                num4 = num4 * (10 ** num_digits) + num[num2[n+1]]          
+        result = num3*num4       
+        print(result) 
 
         
 
 
 if __name__ == "__main__":
     sol = Solution()
-    num1 = "233"
-    num2 = "3"
+    num1 = "20"
+    num2 = "10"
     result = sol.multiply(num1,num2)
     print(result)
