d56fce1dd22ea75c50ba5d76684607152f2f1361
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-17 23:38:35 +0500

    leetcode Solutions

---
+++ 50. Pow(x, n).py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+if __name__ == "__main__":
+    sol = Solution()
+    x = 2.00000
+    n = 10
+    result = sol.myPow(x,n)
+    print(result)
