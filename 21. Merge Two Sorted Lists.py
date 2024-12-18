a998b15e94dc2867ebc1ddd33c03641e61d0e897
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-18 23:53:37 +0500

    leetcode Solutions

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         """
@@ -10,6 +10,7 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        l1 = ListNode
 if __name__ == "__main__":
     sol = Solution()
     list1 = [1,2,4]
