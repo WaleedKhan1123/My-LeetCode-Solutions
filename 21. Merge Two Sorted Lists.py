70161ecefca91d53e5c32050cbacbaa5682ab6d1
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-20 17:23:15 +0500

    leetcode Solutions

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -4,6 +4,7 @@ class ListNode(object):
         self.val = val
         self.next = next
 class Solution(object):
+
     def mergeTwoLists(self, list1, list2):
         """
         :type list1: Optional[ListNode]
@@ -18,16 +19,22 @@ class Solution(object):
         mergelist = []
         for l in range(len(sortlist)-1):
             mergelist.append(ListNode(sortlist[l]))
+
         for l in range(len(mergelist)-1):
             mergelist[l].next = mergelist[l+1]
         # print(f"List is => {mergelist[0].val}")
         c = 0
-        while c < len(mergelist):
+        value = mergelist[0]
+        while value is not None:
             # print(c)
-            print(f"{mergelist[c].val} =>",end="")
+            print(f"{value.val} =>",end="")
+            value = value.next
+            
             c+=1
+        print(c)
             
 if __name__ == "__main__":
+
     sol = Solution()
     list1 = [1,2,4]
     list2 = [1,3,4]
