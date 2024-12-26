0762faa3cd19c554f15d4035b7f6c1686adcd6b6
waleed-khan11 <waleed.kn270@gmail.com>
2024-12-27 01:02:30 +0500

    leetcode Solutions

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -1,12 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution(object):
     def reverseList(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        
+if __name__ == "__main__":
+   if __name__ == "__main__":
+    
+    def create_linked_list(values):
+        dummy = ListNode()
+        current = dummy
+        for val in values:
+            current.next = ListNode(val)
+            current = current.next
+        return dummy.next
+
+    sol = Solution()
+   
+    head = [1,2,3,4,5]
+
+    def linked_list_to_list(node):
+        result = []
+        while node:
+            result.append(node.val)
+            node = node.next
+        return result
+    result = sol.reverseList(head)
+    output = linked_list_to_list(result)
+    print("Merged Linked List:", output)
