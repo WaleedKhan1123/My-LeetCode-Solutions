499d8ecc8d1068ea5477c63581e6408bad315fc5
waleed-khan11 <waleed.kn270@gmail.com>
2025-01-20 00:01:26 +0500

    LeetCode Solutions

---
+++ Add two Numbers II.py
@@ -0,0 +1,69 @@
+# Definition for singly-linked list.
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        Number_String1 = ''
+        Number_String2 = ''
+        
+        while l1:
+            Number_String1 += str(l1.val) 
+            l1 = l1.next
+        Number_String1 = ''.join(reversed(Number_String1)) 
+        
+        while l2:
+            Number_String2+=str(l2.val)
+            l2 = l2.next
+
+        Number_String2 = ''.join(reversed(Number_String2)) 
+        num = int(Number_String1)+int(Number_String2)
+        num = str(num)
+        numlist = []
+        
+        for n in num:
+            numlist.append(int(n))
+        numlist = list(reversed(numlist))
+        dummy = ListNode()
+        current = dummy
+
+        for l in numlist:
+            current.next = ListNode(l)
+            current =  current.next
+        return dummy.next
+
+if __name__ == "__main__":
+
+    sol = Solution()
+    
+    l1 = [2,4,9]
+    
+    l2 = [5,6,4,9]
+    
+    def createLinkedList(list):
+        dummy = ListNode()
+        current = dummy
+        for l in list:
+
+            current.next = ListNode(l)
+            current =  current.next
+        return dummy.next
+            
+    l1 = createLinkedList(l1)
+    l2 = createLinkedList(l2)
+    result = sol.addTwoNumbers(l1,l2)
+
+    while result:
+        print(result.val)
+        result = result.next
+
+    
