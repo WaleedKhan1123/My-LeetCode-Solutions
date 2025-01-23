d0dee657cbd98ef8a8757584b4cde560bcf46215
waleed-khan11 <waleed.kn270@gmail.com>
2025-01-23 21:15:04 +0500

    leetcode

---
+++ Top 150 Interview Questions/27. Remove Element.py
@@ -0,0 +1,15 @@
+#not done yet
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        print(nums)
+        print(val)
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [3,2,2,3]
+    val = 3
+    sol.removeElement(nums,val)
