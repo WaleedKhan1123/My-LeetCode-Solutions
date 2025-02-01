d8d9e878cac17adcce0b701b1a61b5a8489876ee
waleed-khan11 <waleed.kn270@gmail.com>
2025-02-02 01:28:44 +0500

    leetcode solution

--- Top 150 Interview Questions/27. Remove Element.py
+++ Top 150 Interview Questions/27. Remove Element.py
@@ -12,7 +12,13 @@ class Solution(object):
         print(count)
         indexCount =  [i for i,num in enumerate(nums) if num == val]
         print(indexCount)
-
+        i = 0
+        while len(indexCount)>0:
+             
+             nums.pop(indexCount[i])
+             indexCount = [i for i,num in enumerate(nums) if num == val]
+             i-=1
+        print(nums)
         
 if __name__ == "__main__":
     sol = Solution()
