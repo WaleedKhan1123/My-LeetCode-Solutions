b89073871e6a67a3ac9bd6986b97a1a01129f4e2
waleed-khan11 <waleed.kn270@gmail.com>
2025-02-14 15:39:42 +0500

    leetcode solutions

---
+++ Top 150 Interview Questions/26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1,1,2]
+    val = 3
+    sol.removeDuplicates(nums)
