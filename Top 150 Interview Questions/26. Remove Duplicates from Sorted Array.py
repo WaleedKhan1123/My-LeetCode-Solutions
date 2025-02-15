86c4b33e85a52eb1b8cedde39f365cefc07d1181
waleed-khan11 <waleed.kn270@gmail.com>
2025-02-15 23:34:52 +0500

    leetcode solution

--- Top 150 Interview Questions/26. Remove Duplicates from Sorted Array.py
+++ Top 150 Interview Questions/26. Remove Duplicates from Sorted Array.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        print(nums)
+        duplicate = set()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                duplicate.add(nums[i])
+        print(duplicate)
+        
         
 if __name__ == "__main__":
     sol = Solution()
-    nums = [1,1,2]
+    nums = [1,1,2,2,3,4,4,4]
     val = 3
     sol.removeDuplicates(nums)
