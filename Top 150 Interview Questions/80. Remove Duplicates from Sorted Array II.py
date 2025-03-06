067a86ca620f6f0cbd62432f083994ad787bcf4b
waleed-khan11 <waleed.kn270@gmail.com>
2025-03-06 11:32:23 +0500

    leet code solutions

--- Top 150 Interview Questions/80. Remove Duplicates from Sorted Array II.py
+++ Top 150 Interview Questions/80. Remove Duplicates from Sorted Array II.py
@@ -4,7 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        for i in range(len(nums)):
+            
 if __name__ == "__main__":
     sol = Solution()
     nums = [1, 1, 2, 2, 3, 4, 4, 4]
